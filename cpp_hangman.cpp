4dcf02deb1419f3f99edffac1ddb24eb61356f34
githubheang01 <theang2@paragoniu.edu.kh>
2023-08-05 22:12:59 +0700

    get input guess

--- cpp_hangman.cpp
+++ cpp_hangman.cpp
@@ -6,19 +6,47 @@ using namespace std;
 
 class Hangman {
     private:
-        string getRandomWord(){
-            string word;
+        int lives, gamesPlayed;
+        string word, hiddenWord, guess, guessedLetters;
+
+        void randomizeWord(){
             int const wordListSize = 3;
             string wordList[] = {"apple", "banana", "car"};
             srand(time(0));
             word = wordList[rand() % wordListSize];
-            return word;
+        }
+
+        void getPlayerGuess(){
+            while (true){
+                cout << "Guess a letter: ";
+                getline(cin, guess);
+                if (guess.length() > 1) cout << "Only 1 letter allowed!" << endl;
+                else if (guess[0] < 'a' || guess[0] > 'z') cout << "Only lowercase letters allowed!" << endl;
+                else if (guessedLetters.find(guess[0]) != string::npos) cout << "Already guessed that letter!" << endl;
+                else {
+                    guessedLetters += guess[0];
+                    break;
+                }
+            }
         }
 
     public:
         void startGame(){
-            cout << getRandomWord();
+            gamesPlayed++;
+            randomizeWord();
+
+            while(true){
+                getPlayerGuess();
+                hiddenWord = "";
+                for (int i = 0; i < word.length(); i++){
+                    hiddenWord = hiddenWord + "_ ";
+                }
+                cout << guessedLetters;
+            }
+
         }
+
+        
         
 };
 
