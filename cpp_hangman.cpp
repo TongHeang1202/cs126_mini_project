cd5f33d3e169595070e4765df8046844e5174664
githubheang01 <theang2@paragoniu.edu.kh>
2023-08-07 14:38:37 +0700

    main game complete

--- cpp_hangman.cpp
+++ cpp_hangman.cpp
@@ -6,8 +6,9 @@ using namespace std;
 
 class Hangman {
     private:
-        int lives, gamesPlayed;
+        int lives, gamesPlayed, gamesWon;
         string word, hiddenWord, guess, guessedLetters;
+        bool isWin;
 
         void randomizeWord(){
             int const wordListSize = 3;
@@ -16,46 +17,81 @@ class Hangman {
             word = wordList[rand() % wordListSize];
         }
 
-        void getPlayerGuess(){
-            while (true){
-                cout << "Guess a letter: ";
-                getline(cin, guess);
-                if (guess.length() > 1) cout << "Only 1 letter allowed!" << endl;
-                else if (guess[0] < 'a' || guess[0] > 'z') cout << "Only lowercase letters allowed!" << endl;
-                else if (guessedLetters.find(guess[0]) != string::npos) cout << "Already guessed that letter!" << endl;
-                else {
-                    guessedLetters += guess[0];
-                    break;
-                }
-            }
-        }
-
         void displayScreen(){
-            cout << "-----------------------------" << endl;
             cout << "Lives: " << lives << endl;
             hiddenWord = "";
             for (int i = 0; i < word.length(); i++){
-                if (guessedLetters.find(word[i]) != string::npos) hiddenWord = hiddenWord + word[i] + " ";
+                if (guessedLetters.find(word[i]) != string::npos) hiddenWord += word[i];
                 else hiddenWord += "_ ";
             }
             cout << hiddenWord << endl;
         }
 
+        void getPlayerGuess(){
+            while (true){
+                cout << "Guess a letter: ";
+                getline(cin, guess);
+                // Check if guess is allowed
+                if (guess.length() > 1) cout << "Only 1 letter allowed!" << endl;
+                else if (guess[0] < 'a' || guess[0] > 'z') cout << "Only lowercase letters allowed!" << endl;
+                else if (guessedLetters.find(guess[0]) != string::npos) cout << "Already guessed that letter!" << endl;
+                else break;
+            }
+        }
+
+        void checkPlayerGuess(){
+            if (word.find(guess[0]) != string::npos){
+                cout << "Correct Guess!" << endl;
+                guessedLetters += guess[0];
+
+                isWin = true;
+                for (int i = 0; i < word.length(); i++){
+                    if (guessedLetters.find(word[i]) == string::npos) isWin = false;
+                }
+            }
+            else {
+                cout << "Wrong Guess!" << endl;
+                guessedLetters += guess[0];
+                lives--;
+            }
+        }
+
+        bool checkGameEnd(){
+            if (lives == 0){
+                cout << "-----------------------------" << endl;
+                cout << "YOU LOST!" << endl;
+                cout << "The word is: " << word << endl;
+                return true;
+            }
+            else if (isWin){
+                cout << "-----------------------------" << endl;
+                cout << "YOU WON!" << endl;
+                gamesWon++;
+                return true;
+            }
+            return false;
+        }
+
     public:
+        Hangman(){
+            gamesPlayed = 0;
+            gamesWon = 0;
+        }
+
         void startGame(){
             lives = 7;
             gamesPlayed++;
             randomizeWord();
 
             while(true){
+                cout << "-----------------------------" << endl;
                 displayScreen();
                 getPlayerGuess();
+                checkPlayerGuess();
+                if (checkGameEnd()) break;
+                cout << "-----------------------------" << endl;
             }
-
         }
-
-        
-        
 };
 
 int main(){
